c07cdc811bfab7c49df60d9e698e91b4e3ef4213
himanshu02102004 <himanshu2104mishra@gmail.com>
2025-01-29 18:24:58 +0530

    New commit message

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+const apiUrl = process.env.API_URL || 'https://waters-gr9rkdg12-himanshus-projects-12d724e0.vercel.app'; // Fallback to local if no env variable
+
 document.getElementById('homeButton').addEventListener('click', function() {
     showSection('homeSection');
 });
@@ -14,7 +16,7 @@ function showSection(sectionId) {
     document.getElementById('homeSection').classList.add('hidden');
     document.getElementById('loginSection').classList.add('hidden');
     document.getElementById('registerSection').classList.add('hidden');
-    
+
     document.getElementById(sectionId).classList.remove('hidden');
 }
 
@@ -41,7 +43,7 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         alert('Passwords do not match!');
     } else {
         alert('Registration Succesfull');
-        
+
         // Add registration logic here
     }
 });
@@ -57,8 +59,7 @@ document.getElementById('grievanceForm').addEventListener('submit', function(eve
 });
 
 // Simulated data for grievance status (replace with actual data handling)
-let grievanceData = [
-    {
+let grievanceData = [{
         fullname: 'John Doe',
         address: '123 Main St, City, Country',
         filedTime: '2024-07-16 10:30 AM',
@@ -113,106 +114,105 @@ function showMap() {
     }).addTo(map);
 
     // Sample water location details
-    var waterLocations = [
-        {
-            photo:'image copy.png',
+    var waterLocations = [{
+            photo: 'image copy.png',
             owner: 'Sanket Yalwar',
             address: 'BLDEACET, Boys Hostel, Ashram Road, Vijayapur',
-            phone:'9591556007',
+            phone: '9591556007',
             type: 'Pumping Station',
             latlng: [16.848413, 75.717253] // Latitude and Longitude
         },
         {
-            photo:'https://lh5.googleusercontent.com/p/AF1QipNy3uQ98UpElf2bH6_Yxzoe_GBhFl2bkfMHqPwZ=w408-h306-k-no',
+            photo: 'https://lh5.googleusercontent.com/p/AF1QipNy3uQ98UpElf2bH6_Yxzoe_GBhFl2bkfMHqPwZ=w408-h306-k-no',
             owner: 'Omkar Cholli',
-            address:'Aishwarya Nagar, Neela Nagar, Vijayapura',
-            phone:'6362316300',
+            address: 'Aishwarya Nagar, Neela Nagar, Vijayapura',
+            phone: '6362316300',
             type: 'Pumping Station',
             latlng: [16.850690, 75.722203] // Latitude and Longitude
         },
         {
-            photo:'',
+            photo: '',
             owner: 'Sankalp H',
-            address:'KC Nagar, Adarsh Nagar, Vijayapura',
-            phone:'9901464092',
+            address: 'KC Nagar, Adarsh Nagar, Vijayapura',
+            phone: '9901464092',
             type: 'Pumping Station',
             latlng: [16.857729, 75.709043] // Latitude and Longitude
         },
         {
-            photo:'https://lh5.googleusercontent.com/p/AF1QipPzcbyZeFoDdgyFzWR5xu5iaUWQw1W5gKSqiRHI=w408-h306-k-no',
+            photo: 'https://lh5.googleusercontent.com/p/AF1QipPzcbyZeFoDdgyFzWR5xu5iaUWQw1W5gKSqiRHI=w408-h306-k-no',
             owner: 'Savidhare Minerals(Savi Water)',
-            address:'Solapur Rd, opp. GOVT ITI COLLEGE, Chalukya Nagar, Karnataka Housing Board Colony, Vijayapura',
-            phone:'9845820401',
+            address: 'Solapur Rd, opp. GOVT ITI COLLEGE, Chalukya Nagar, Karnataka Housing Board Colony, Vijayapura',
+            phone: '9845820401',
             type: 'Pumping Station',
             latlng: [16.843497, 75.709072] // Latitude and Longitude
         },
         {
             photo: 'https://www.google.com/maps/place/Water+Tank/@16.8061478,75.7206726,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNkDjk8OOqvoYYBsUfZ1ieA-NqUQ3DbnFFuJLwd!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNkDjk8OOqvoYYBsUfZ1ieA-NqUQ3DbnFFuJLwd%3Dw191-h86-k-no!7i4000!8i1800!4m11!1m2!2m1!1swater+supply+pipelines+in+bijapur+district!3m7!1s0x3bc6555c30369cbb:0x11503ab915f1129a!8m2!3d16.8061651!4d75.7205073!10e5!15sCip3YXRlciBzdXBwbHkgcGlwZWxpbmVzIGluIGJpamFwdXIgZGlzdHJpY3RaLCIqd2F0ZXIgc3VwcGx5IHBpcGVsaW5lcyBpbiBiaWphcHVyIGRpc3RyaWN0kgETaGlzdG9yaWNhbF9sYW5kbWFya5oBJENoZERTVWhOTUc5blMwVkpRMEZuU1VOVk5UVllkVzluUlJBQuABAA!16s%2Fg%2F11c2nyr6q1?entry=ttu#',
             owner: 'Water Tank',
-            address:'RP4C+F66, Jala Nagar, Vijayapura, Karnataka 586109, India',
-            phone:'6362316300',
+            address: 'RP4C+F66, Jala Nagar, Vijayapura, Karnataka 586109, India',
+            phone: '6362316300',
             type: 'Water Supply Pipelines',
-            latlng: [16.8054798,75.7170889] // Latitude and Longitude
+            latlng: [16.8054798, 75.7170889] // Latitude and Longitude
         },
         {
-            photo:'https://lh5.googleusercontent.com/p/AF1QipPTPyXCrHBdQSVUoRn1rWu6ko07OEyYr4cONIyV=w408-h612-k-no',
+            photo: 'https://lh5.googleusercontent.com/p/AF1QipPTPyXCrHBdQSVUoRn1rWu6ko07OEyYr4cONIyV=w408-h612-k-no',
             owner: 'Runwal Machinery & Auto Store',
-            address:'185 a /b, Mahaveer Rd, near jio cc bank, Sholapur, Vijayapura',
-            phone:'8549085338',
+            address: '185 a /b, Mahaveer Rd, near jio cc bank, Sholapur, Vijayapura',
+            phone: '8549085338',
             type: 'Pumping Station',
             latlng: [16.832099, 75.715105] // Latitude and Longitude
         },
         {
-            photo:'https://lh5.googleusercontent.com/p/AF1QipOkcBqi2rFUcwquDfa4f23BFSoenuLKqRRGmzHr=w408-h306-k-no',
+            photo: 'https://lh5.googleusercontent.com/p/AF1QipOkcBqi2rFUcwquDfa4f23BFSoenuLKqRRGmzHr=w408-h306-k-no',
             owner: 'Karnataka Urban Water Supply And Drainage Board',
-            address:'RPGG+J8V, Badi Kaman Rd, Vijayapura',
-            phone:'8863589103',
+            address: 'RPGG+J8V, Badi Kaman Rd, Vijayapura',
+            phone: '8863589103',
             type: 'Urban Water Supply and Drainage',
             latlng: [16.826639, 75.725538] // Latitude and Longitude
         },
         {
-            photo:'',
+            photo: '',
             owner: 'STP- Sewage Treatment Plant',
-            address:'QPMP+7WH, Vijayapura, Karnataka 586102, India',
-            phone:'9148682703',
+            address: 'QPMP+7WH, Vijayapura, Karnataka 586102, India',
+            phone: '9148682703',
             type: 'Urban Water Supply and Drainage',
             latlng: [16.782946, 75.737137] // Latitude and Longitude
         },
-        
+
         {
-            photo:'https://lh5.googleusercontent.com/p/AF1QipMA0HE8_a36f4Xee5gZpUkvR3vc7gc90eSk29np=w408-h544-k-no',
+            photo: 'https://lh5.googleusercontent.com/p/AF1QipMA0HE8_a36f4Xee5gZpUkvR3vc7gc90eSk29np=w408-h544-k-no',
             owner: 'Begam Talab Water Supply Pipe',
-            address:'QPR2+44Q, Vijayapura, Karnataka 586102, India',
-            phone:'798657657',
+            address: 'QPR2+44Q, Vijayapura, Karnataka 586102, India',
+            phone: '798657657',
             type: 'Pipeline',
-            latlng: [16.8368072,75.636286] // Latitude and Longitude
+            latlng: [16.8368072, 75.636286] // Latitude and Longitude
         },
         {
             photo: 'https://lh5.googleusercontent.com/p/AF1QipPXRgD9hoT9Lrb1-L5l3QM8Vm8r4af9VeNyA2eB=w426-h240-k-no',
             owner: 'MAHAGURU DISTRIBUTORS',
-            address:'Shahapeti Road, near Indian Bank, Vijayapura, Karnataka 586101, India',
-            phone:'6587980675',
+            address: 'Shahapeti Road, near Indian Bank, Vijayapura, Karnataka 586101, India',
+            phone: '6587980675',
             type: 'plumbing Supply Store',
-            latlng: [16.8258325,75.6953346] // Latitude and Longitude
+            latlng: [16.8258325, 75.6953346] // Latitude and Longitude
         },
         {
-            photo:'https://lh5.googleusercontent.com/p/AF1QipOBRjEcxYE4Xhb0bhihmKP0D4b5Fo9jRhc6BN-x=w426-h240-k-no',
+            photo: 'https://lh5.googleusercontent.com/p/AF1QipOBRjEcxYE4Xhb0bhihmKP0D4b5Fo9jRhc6BN-x=w426-h240-k-no',
             owner: 'Lake Water Supply Pipe',
-            address:'VPM7+8M3, Vijayapura, Karnataka 586103, India',
-            phone:'6759479898',
+            address: 'VPM7+8M3, Vijayapura, Karnataka 586103, India',
+            phone: '6759479898',
             type: 'Urban Water Supply and Drainage',
-            latlng: [16.8832597,75.5718429] // Latitude and Longitude
+            latlng: [16.8832597, 75.5718429] // Latitude and Longitude
         },
         {
-            photo:'https://lh5.googleusercontent.com/p/AF1QipOijRWPOvdVGB0YwoUnOTSQPA5H-ovmUffxc73F=w408-h306-k-no',
+            photo: 'https://lh5.googleusercontent.com/p/AF1QipOijRWPOvdVGB0YwoUnOTSQPA5H-ovmUffxc73F=w408-h306-k-no',
             owner: 'SHREE RAMAKRISHNA ENTERPRISES',
-            address:'Jyoti Pipe Factory, Front Road, beside Water Recycling Plant, near Appaji Hospital, Vijayapura, Karnataka 586103, India',
-            phone:'7869587685',
+            address: 'Jyoti Pipe Factory, Front Road, beside Water Recycling Plant, near Appaji Hospital, Vijayapura, Karnataka 586103, India',
+            phone: '7869587685',
             type: 'Pipe Factory Manufacturer',
-            latlng: [16.8367743,75.6362858] // Latitude and Longitude
+            latlng: [16.8367743, 75.6362858] // Latitude and Longitude
         },
-        
-        
+
+
 
     ];
 
@@ -223,17 +223,17 @@ function showMap() {
             .bindPopup(
                 '<img src="' + location.photo + '" alt="Water Location Photo" style="width:100px;height:100px;"/>' + '<br>' +
                 '<b>Owner:</b> ' + location.owner + '<br>' +
-                '<b>Address:</b> ' + location.address + '<br>'+
-                '<b>Phone:</b> ' + location.phone + '<br>' + 
-                '<b>Type:</b> ' + location.type 
-                )
+                '<b>Address:</b> ' + location.address + '<br>' +
+                '<b>Phone:</b> ' + location.phone + '<br>' +
+                '<b>Type:</b> ' + location.type
+            )
             .openPopup();
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     // Fetch data for the water supply and display it on the map
-    fetch('http://127.0.0.1:8000/api/water-supply/')
+    fetch(`${apiUrl}/api/water-supply/`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -247,37 +247,37 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => console.error('Error:', error));
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     // Handle grievance form submission
     document.getElementById('grievanceForm').addEventListener('submit', function(e) {
         e.preventDefault();
-        
+
         let formData = new FormData(this);
-        
-        fetch('http://127.0.0.1:8000/api/grievances/', {
-            method: 'POST',
-            body: JSON.stringify({
-                user: formData.get('user'), // Adjust according to your form fields
-                description: formData.get('description'),
-                status: 'Pending'
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            // Handle success (e.g., show a success message or update the UI)
-            alert('Grievance submitted successfully!');
-        })
-        .catch(error => console.error('Error:', error));
+
+        fetch(`${apiUrl}/api/water-supply/`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    user: formData.get('user'), // Adjust according to your form fields
+                    description: formData.get('description'),
+                    status: 'Pending'
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Handle success (e.g., show a success message or update the UI)
+                alert('Grievance submitted successfully!');
+            })
+            .catch(error => console.error('Error:', error));
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     // Fetch data for water supply
-    fetch('http://127.0.0.1:8000/api/water-supply/')
+    fetch(`${apiUrl}/api/water-supply/`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -296,24 +296,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
         let formData = new FormData(this);
 
-        fetch('http://127.0.0.1:8000/api/grievances/', {
-            method: 'POST',
-            body: JSON.stringify({
-                user: formData.get('user'), // Adjust according to your form fields
-                description: formData.get('description'),
-                status: 'Pending'
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            // Handle success (e.g., show a success message or update the UI)
-            alert('Grievance submitted successfully!');
-        })
-        .catch(error => console.error('Error:', error));
+        fetch(`${apiUrl}/api/water-supply/`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    user: formData.get('user'), // Adjust according to your form fields
+                    description: formData.get('description'),
+                    status: 'Pending'
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Handle success (e.g., show a success message or update the UI)
+                alert('Grievance submitted successfully!');
+            })
+            .catch(error => console.error('Error:', error));
     });
 });
 
@@ -366,8 +366,3 @@ document.getElementById("statusButton").addEventListener("click", function() {
 document.getElementById("logoutButton").addEventListener("click", function() {
     // Handle click event for logout button
 });
-
-
-
-
-
